8a7a21fb6ec12389d53f19a5e6ff766401630596
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-03-25 12:13:32 +0000

    Update HowToPlay.java

--- HowToPlay.java
+++ HowToPlay.java
@@ -4,7 +4,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.geometry.Insets;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
