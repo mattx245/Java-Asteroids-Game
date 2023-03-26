69b7a0c848350234a3626deebf1a08123c9725f3
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-03-26 23:54:44 +0100

    Added Scene Text

--- Game.java
+++ Game.java
@@ -8,6 +8,10 @@ import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
 
 import java.io.IOException;
 
@@ -16,7 +20,9 @@ public class Game extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Pane pane = new Pane();
-        pane.setPrefSize(950, 700);
+        int prefWidth = 950;
+        int prefHeight = 700;
+        pane.setPrefSize(prefWidth, prefHeight);
 
         // Rocket
         //Polygon ship = new Polygon(-5, -10, 25, 0, -5, 10);
@@ -30,6 +36,38 @@ public class Game extends Application {
 
         Scene scene = new Scene(pane);
         stage.setTitle("Asteroid Game");
+
+        // Text score
+        Text score = new Text();
+        score.setText("Score: 0000");
+        score.setFont(Font.font(20));
+        score.setFill(Color.WHITE);
+
+        // Text score
+        Text help = new Text();
+        help.setText("Help");
+        help.setFont(Font.font(20));
+        help.setFill(Color.WHITE);
+
+        // Create VBox to hold score
+        VBox vbox_score = new VBox(score);
+        vbox_score.setAlignment(Pos.CENTER);
+        vbox_score.setPrefWidth(prefWidth);
+
+        // Create VBox to hold help
+        VBox vbox_help = new VBox(help);
+        vbox_help.setAlignment(Pos.CENTER);
+
+        // Add VBox to Pane
+        pane.getChildren().add(vbox_score);
+        pane.getChildren().add(vbox_help);
+
+        // Set position of VBox to top of Pane
+        vbox_score.setLayoutY(20);
+        vbox_help.setLayoutY(20);
+        vbox_help.setLayoutX(880);
+
+
         stage.setScene(scene);
         scene.setFill(Color.BLACK);
         stage.show();
