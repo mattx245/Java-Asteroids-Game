18b46adf489b82422f73cfd766e574b2c4d491ab
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-04-10 13:35:38 +0100

    Initial changes to Asteroid

--- src/main/java/com/asteroids/javaasteroidsgame/Asteroid.java
+++ src/main/java/com/asteroids/javaasteroidsgame/Asteroid.java
@@ -1,13 +1,21 @@
 package com.asteroids.javaasteroidsgame;
+import javafx.scene.shape.Polygon;
+
 import java.util.Random;
 
 public class Asteroid extends Character {
 
     private double rotationalMovement;
+    private String size;
 
     public Asteroid(int x, int y) {
-        super(new PolygonFactory().createPolygon(), x, y);
 
+        //large: slow -> 2M
+        //medium: faster -> 2S
+        //small: even faster -> destroyed
+
+        super(new PolygonFactory().createPolygon(), x, y);
+        size = "Large"
         Random rnd = new Random();
 
         super.getCharacter().setRotate(rnd.nextInt(360));
@@ -18,6 +26,49 @@ public class Asteroid extends Character {
         }
 
         this.rotationalMovement = 0.5 - rnd.nextDouble();
+    }
+
+    public Asteroid(int x, int y, String size) {
+        super(new PolygonFactory().createPolygon(), x, y);
+        Random rnd = new Random();
+
+        super.getCharacter().setRotate(rnd.nextInt(360));
+
+        int accelerationAmount = 1 + rnd.nextInt(10);
+        for (int i = 0; i < accelerationAmount; i++) {
+            accelerate();
+        }
+
+        this.rotationalMovement = 0.5 - rnd.nextDouble();
+    }
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public Polygon createAsteroid(String size) {
+        Random rnd = new Random();
+
+        double size = 10 + rnd.nextInt(10);
+
+        Polygon polygon = new Polygon();
+        double c1 = Math.cos(Math.PI * 2 / 5);
+        double c2 = Math.cos(Math.PI / 5);
+        double s1 = Math.sin(Math.PI * 2 / 5);
+        double s2 = Math.sin(Math.PI * 4 / 5);
+
+        polygon.getPoints().addAll(
+                size, 0.0,
+                size * c1, -1 * size * s1,
+                -1 * size * c2, -1 * size * s2,
+                -1 * size * c2, size * s2,
+                size * c1, size * s1);
+
+        for (int i = 0; i < polygon.getPoints().size(); i++) {
+            int change = rnd.nextInt(5) - 2;
+            polygon.getPoints().set(i, polygon.getPoints().get(i) + change);
+        }
+
+        return polygon;
     }
 
     @Override
