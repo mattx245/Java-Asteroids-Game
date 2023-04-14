adb8b00627a6653858684f529f3fbb67058af16e
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-04-14 12:20:07 +0100

    Adding limited shooting distance

--- src/main/java/com/asteroids/javaasteroidsgame/Projectile.java
+++ src/main/java/com/asteroids/javaasteroidsgame/Projectile.java
@@ -4,9 +4,20 @@ import javafx.scene.shape.Polygon;
 
 public class Projectile extends Character {
 
+    private int shooting_distance;
 
     public Projectile(int x, int y) {
         super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
+        shooting_distance = 100;
     }
 
+    @Override
+    public void move() {
+        if(shooting_distance < 1) {
+            this.setAlive(false);
+        } else {
+            super.move();
+            shooting_distance -= 1;
+        }
+    }
 }
