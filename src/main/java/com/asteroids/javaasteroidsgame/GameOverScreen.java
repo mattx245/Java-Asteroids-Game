a34696922280472d299725b7093695c93d3b529a
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-04-21 09:25:09 +0100

    Revert "Finished game over screen - Modified AsteroidsApplication so that it can pass the players score to gameover."
    This reverts commit d2afb8793390670d263025d905a8fa34ff6978af, reversing
    changes made to 5522e3adaafc0eaaffda158a02ef74cc83e4f68a.

--- src/main/java/com/asteroids/javaasteroidsgame/GameOverScreen.java
+++ src/main/java/com/asteroids/javaasteroidsgame/GameOverScreen.java
@@ -21,7 +21,6 @@ import javafx.scene.layout.VBox;
 
 public class GameOverScreen extends HighScore {
     public int playerScore;
-
     private void createStars(Pane pane, int numStars) {
         Random random = new Random();
 
